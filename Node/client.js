eefd158f9a1dd77c228ad2b9b411aa2159c3983c
Antonio Mika <antoniomika@gmail.com>
2016-04-07 10:53:53 -0400

    Updates

--- Node/client.js
+++ Node/client.js
@@ -6,11 +6,11 @@ var ws = new WebSocket("ws://" + process.argv[2] + "/ws/stream");
 
 ws.on('open', function() {
     console.log("Opened!");
-    var child = spawn("/usr/bin/raspivid", ["-t", "0", "-w", "300", "-h", "300", "-hf", "-fps", "20", "-o", "-"]);
+    var child = spawn("python", ["../Python/camera.py"]);
     child.stdout.on("data", function(data) {
         ws.send(JSON.stringify({
             "time": new Date().getTime(),
-            "data": data.toString("base64")
+            "data": data
         }));
     });
 });
--- Python/camera.py
+++ Python/camera.py
@@ -2,27 +2,36 @@ import io
 import sys
 import time
 import base64
-import server
 import picamera
-import threading
-from websocket import create_connection
 
+"""
+import server
+import threading
+import websocket
 
 def sendData():
     ws = create_connection("ws://" + sys.argv[1] + "/ws/stream")
     while True:
         if server.lastFrame is not None:
             ws.send(base64.b64encode(server.lastFrame))
+            ws.recv()
 
 lastFrame = ""
 
 webserverThread = threading.Thread(target=server.app.run,
                                    kwargs=dict(threaded=True, host="0.0.0.0",
-                                               port=8080))
+                                               port=9090))
 webserverThread.start()
 
 wsThread = threading.Thread(target=sendData)
 wsThread.start()
+
+ws = create_connection("ws://" + sys.argv[1] + "/ws/stream")
+
+ws = websocket.WebSocketApp("ws://" + sys.argv[1] + "/ws/stream")
+wst = threading.Thread(target=ws.run_forever)
+wst.daemon = True
+wst.start()"""
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1080, 720)
@@ -33,9 +42,8 @@ with picamera.PiCamera() as camera:
     stream = io.BytesIO()
     for frame in camera.capture_continuous(stream, format="jpeg",
                                            use_video_port=True):
-
         stream.seek(0)
-        server.lastFrame = stream.read()
+        print base64.b64encode(stream.read())
 
         stream.seek(0)
         stream.truncate()
